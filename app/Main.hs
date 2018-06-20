96b8cf1798f797038244136d528d6a835671fb8c
Hexirp <Hexirp@users.noreply.github.com>
2018-06-20 17:23:12 +0900

    Fix: 単純なミスを

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
  import Prelude
+
+ import Data.Word
 
  import qualified SDL
 
  main :: IO ()
  main = do
   SDL.initialize []
-  w <- SDL.createWindow title defaultWindow
+  w <- SDL.createWindow "Hello, SDL2!" SDL.defaultWindow
   SDL.showWindow w
   ws <- SDL.getWindowSurface w
   SDL.surfaceFillRect ws Nothing winColor
@@ -16,5 +20,5 @@ module Main where
   SDL.destroyWindow w
   SDL.quit
 
- winColor :: SDL.V4
+ winColor :: SDL.V4 Word8
  winColor = SDL.V4 maxBound maxBound maxBound maxBound
