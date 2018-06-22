40a50bcd4451ec3bbbf2b12461fb5f637af15a8a
Hexirp <Hexirp@users.noreply.github.com>
2018-06-22 19:36:37 +0900

    Fix: 小さすぎる＆遅すぎる

--- app/Main.hs
+++ app/Main.hs
@@ -4,17 +4,17 @@ module Main where
  import Graphics.Gloss
 
  main :: IO ()
- main = simulate di white 10 0 vi (\_ _ m -> m + 0.05)
+ main = simulate di white sp 0 vi (\_ _ m -> m + 0.05)
 
  di :: Display
  di = InWindow "I see you." (400, 400) (200, 200)
 
  sp :: Int
- sp = 10
+ sp = 30
 
  vi :: Float -> Picture
  vi m = Rotate m $ Polygon [
   (0, 0),
-  (0, 10),
-  (10, 10),
-  (10, 0)]
+  (0, 50),
+  (50, 50),
+  (50, 0)]
