7908abde43e000ea481fbd9094c6a4f0a670cc16
Hexirp <Hexirp@users.noreply.github.com>
2018-06-22 19:34:11 +0900

    Fix: シミュレーション速度を渡し忘れたのを

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,13 @@ module Main where
  import Graphics.Gloss
 
  main :: IO ()
- main = simulate di white 0 vi (\_ _ m -> m + 0.05)
+ main = simulate di white 10 0 vi (\_ _ m -> m + 0.05)
 
  di :: Display
  di = InWindow "I see you." (400, 400) (200, 200)
+
+ sp :: Int
+ sp = 10
 
  vi :: Float -> Picture
  vi m = Rotate m $ Polygon [
