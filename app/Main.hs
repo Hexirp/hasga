128306c98ebf2dd90f1fc04f1110ba7c3865642f
Hexirp <Hexirp@users.noreply.github.com>
2018-12-27 15:25:57 +0900

    Fix: 境界での挙動がおかしい
    Wordが負の数を扱えないためだと推測してIntに変更。

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ module Main where
 
  import Data.List (intercalate)
 
- import Data.Word
+ import Data.Int
 
  import Data.Ix
  import Data.Array.IArray
@@ -38,7 +38,7 @@ module Main where
  -- z = (1, 0)
  -- w = (1, 2)
  -- v = (2, 1)
- type GameState = UArray (Word8, Word8) Bool
+ type GameState = UArray (Int8, Int8) Bool
 
  -- | Build array by function to compute elements from index
  arrayByIndex
@@ -49,15 +49,15 @@ module Main where
  arrayByIndex bd f = array bd [ (i, f i) | i <- range bd ]
 
  -- | Width of x
- xWidth :: Word8
+ xWidth :: Int8
  xWidth = 16
 
  -- | Width of y
- yWidth :: Word8
+ yWidth :: Int8
  yWidth = 17
 
  -- | Size of the field
- fieldSize :: ((Word8, Word8), (Word8, Word8))
+ fieldSize :: ((Int8, Int8), (Int8, Int8))
  fieldSize = ((0, 0), (xWidth - 1, yWidth - 1))
 
  -- | Initial state of the game
@@ -67,7 +67,7 @@ module Main where
    -- -#-
    -- --#
    -- ###
-   f :: Word8 -> Word8 -> Bool
+   f :: Int8 -> Int8 -> Bool
    f 0 1 = True
    f 1 2 = True
    f 2 0 = True
@@ -79,7 +79,7 @@ module Main where
  updateGameState :: GameState -> GameState
  updateGameState a = arrayByIndex fieldSize (uncurry f)
   where
-   f :: Word8 -> Word8 -> Bool
+   f :: Int8 -> Int8 -> Bool
    f x y = let
      n0 = g (x - 1, y - 1)
      n1 = g (x - 1, y    )
@@ -93,7 +93,7 @@ module Main where
      n = length $ filter id [n0, n1, n2, n3, n5, n6, n7, n8]
     in
      if n4 then n == 2 || n == 3 else n == 3
-   g :: (Word8, Word8) -> Bool
+   g :: (Int8, Int8) -> Bool
    g (x, y) = a ! (x `mod` xWidth, y `mod` yWidth)
 
  -- | View state of the game
@@ -104,7 +104,7 @@ module Main where
    b2c x = if x then '#' else '-'
    xv :: [[Bool]]
    xv = [ yv x | x <- [ 0 .. xWidth - 1] ]
-   yv :: Word8 -> [Bool]
+   yv :: Int8 -> [Bool]
    yv x = [ a ! (x, y) | y <- [ 0 .. yWidth - 1 ] ]
    f :: [[Bool]] -> String
    f []       = ""
@@ -112,3 +112,7 @@ module Main where
    g :: [Bool] -> String
    g []       = ""
    g (x : xs) = ' ' : b2c x : g xs
+
+ -- References:
+ -- - https://github.com/Hexirp/haskell-gist/blob/f940d84bd79c2d6d9f05ad68f93152abeeed6297/Lifegame.hs
+ -- - https://qiita.com/lotz/items/bdb04c771efc8919b79c
