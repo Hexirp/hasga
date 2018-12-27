911a79e7a7c17af74e8db1d818d683dc24743148
Hexirp <Hexirp@users.noreply.github.com>
2018-12-27 12:58:04 +0900

    Add: updateGameState

--- app/Main.hs
+++ app/Main.hs
@@ -39,3 +39,24 @@ module Main where
    f 2 1 = True
    f 2 2 = True
    f _ _ = False
+
+ -- | Update state of the game
+ updateGameState :: GameState -> GameState
+ updateGameState a = arrayByIndex fieldSize (uncurry f)
+  where
+   f :: Word8 -> Word8 -> Bool
+   f x y = let
+     n0 = g (x - 1, y - 1)
+     n1 = g (x - 1, y    )
+     n2 = g (x - 1, y + 1)
+     n3 = g (x    , y - 1)
+     n4 = g (x    , y    )
+     n5 = g (x    , y + 1)
+     n6 = g (x + 1, y - 1)
+     n7 = g (x + 1, y    )
+     n8 = g (x + 1, y + 1)
+     n = length $ filter id [n0, n1, n2, n3, n5, n6, n7, n8]
+    in
+     if n4 then n == 2 || n == 3 else n == 3
+   g :: (Word8, Word8) -> Bool
+   g (x, y) = a ! (x `mod` 64, y `mod` 64)
