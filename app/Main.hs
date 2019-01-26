3885104ae0ebf837bf1c540d30c9173eb0292cd0
Hexirp <Hexirp@users.noreply.github.com>
2019-01-26 12:49:29 +0900

    Update: 名前を間違ってた

--- app/Main.hs
+++ app/Main.hs
@@ -5,13 +5,13 @@ module Main where
  main :: IO ()
  main = do
   s <- readLn :: IO Int
-  putStrLn $ take 20 $ drop (s - 1) $ fizz_bizz_string
+  putStrLn $ take 20 $ drop (s - 1) $ fizz_buzz_string
 
- fizz_bizz_string :: String
- fizz_bizz_string = concat $ map fizz_bizz [1..]
+ fizz_buzz_string :: String
+ fizz_buzz_string = concat $ map fizz_buzz [1..]
 
- fizz_bizz :: Int -> String
- fizz_bizz n
+ fizz_buzz :: Int -> String
+ fizz_buzz n
   | n `mod` 3 == 0 && n `mod` 5 == 0 = "FizzBuzz"
   | n `mod` 3 == 0                   = "Fizz"
   |                   n `mod` 5 == 0 = "Buzz"
